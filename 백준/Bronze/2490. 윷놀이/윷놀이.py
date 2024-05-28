ff8b4217dea14c51413412211c747ee0a9b3adf0
jxxlxk <mejadenlee@gmail.com>
2024-05-28 16:23:46 +0900

    [Bronze III] Title: 윷놀이, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2490. 윷놀이/윷놀이.py
@@ -0,0 +1,13 @@
+for i in range(3):
+  g = list(input())
+  a = g.count('0')
+  if a == 1:
+    print('A')
+  elif a == 2:
+    print('B')
+  elif a == 3:
+    print('C')
+  elif a ==4:
+    print('D')
+  else:
+    print('E')
