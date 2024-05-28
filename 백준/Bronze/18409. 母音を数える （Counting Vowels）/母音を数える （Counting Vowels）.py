82016f4e3e8129d26f769725c9fffd09f50825e3
jxxlxk <mejadenlee@gmail.com>
2024-05-28 22:56:29 +0900

    [Bronze V] Title: 母音を数える (Counting Vowels), Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/18409. 母音を数える （Counting Vowels）/母音を数える （Counting Vowels）.py
@@ -0,0 +1,15 @@
+a = int(input())
+cnt = 0
+b = input()
+for i in range(a):
+    if b[i] == 'a' or b[i] =='e' or b[i] =='i' or b[i] =='u' or b[i] =='o':
+        cnt += 1
+        
+        
+        
+        
+        
+print(cnt)
+    
+    
+    
