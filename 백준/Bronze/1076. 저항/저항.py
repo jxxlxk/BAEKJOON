0507ed210609f52322e75560b978df5440e824fa
jxxlxk <mejadenlee@gmail.com>
2024-05-28 15:57:19 +0900

    [Bronze II] Title: 저항, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,7 @@
+color = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+
+a = int(color.index(input()))
+b = int(color.index(input()))
+c = int(color.index(input()))
+
+print( (a*10 + b) * (10**c) ) 
